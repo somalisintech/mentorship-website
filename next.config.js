3007e3a9a28364f1523fc6e39f11c783b2d3c652
Abshir Ahmed <abshir.ahmed@cazoo.co.uk>
2022-06-06 22:26:57 +0100

    enable swcMinify
    see https://nextjs.org/blog/next-12-1#faster-minification-with-swc

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const config = {
   productionBrowserSourceMaps: true,
-  reactStrictMode: true
+  reactStrictMode: true,
+  swcMinify: true
 };
 
 module.exports = config;
