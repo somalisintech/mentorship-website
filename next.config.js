18a5e3e43a6137206d22a67ad47ee613342dee51
Abshir Ahmed <abshir.ahmed@cazoo.co.uk>
2022-06-07 13:10:40 +0100

    :white_check_mark: include global styling to cypress component tests

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { patchWebpackConfig } = require('next-global-css');
+
 /** @type {import('next').NextConfig} */
 const config = {
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
-  swcMinify: true,
   compiler: {
     removeConsole: true
+  },
+  webpack: (config, options) => {
+    if (process.env.CYPRESS === 'true') {
+      //Allows importing the global.css file in cypress/support/component.ts
+      patchWebpackConfig(config, options);
+    }
+    return config;
   }
 };
 
